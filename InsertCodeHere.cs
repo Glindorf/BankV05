1aa153a0737e7a029029baab221bc338d178f410
Madeleine Glindorf <mglindorf@gmail.com>
2020-03-30 13:22:55 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BankV05
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            BankAccount myAccount = new BankAccount("Jeppe");
+
+            myAccount.Deposit(2000);
+            Console.WriteLine($"{myAccount.AccountHolderName}'s account balance is: {myAccount.Balance}");
+
+            myAccount.Withdraw(2500);
+            Console.WriteLine($"{myAccount.AccountHolderName}'s account balance is: {myAccount.Balance}");
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BankV05
+{
+    class Program
+    {
+        /// <summary>
+        /// Her er en lille test, den er sød 
+        /// </summary>
+        static void Main(string[] args)
+        {
+            InsertCodeHere theCode = new InsertCodeHere();
+            theCode.MyCode();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close the program...");
+
+            Console.ReadKey();
+        }
+    }
+}
